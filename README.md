64160c05a1f546e56bb00907022c797a081e3f41
dgf <deep@dagnu.de>
2014-12-28 13:25:04 +0100

    import simple repo management

---
+++ README.md
@@ -0,0 +1,4 @@
+# Digital MemeX Repository Management
+
+This installs (clones) and updates (pull) remote application (Git) repositories
+and is itself an example of an external repository.
---
+++ repositories.js
@@ -0,0 +1,24 @@
+/*global $, RESTClient*/
+
+$(function() { // jQuery ready => start up
+  var dmc = new RESTClient('/core'),
+      repositories = dmc.get_topics('dmx.repository', true, true),
+      $table = $('#repositories').empty(),
+      renderRepository = function (t) {
+        var name = t.childs['dmx.repository.name'].value,
+            desc = t.childs['dmx.repository.description'].value,
+            status = t.childs['dmx.repository.status'].uri,
+            $row = $('<tr>');
+        if (status === 'dmx.repository.status.installed') {
+          $row.append($('<td>').append($('<a>').text(name).attr('href', '/dmx/application/' + name)));
+          $row.append($('<td>').text(desc));
+          $row.append($('<td>').append($('<a>').text('update').attr('href', '/dmx/repository/' + name + '/pull')));
+        } else { // configured?
+          $row.append($('<td>').text(name));
+          $row.append($('<td>').text(desc));
+          $row.append($('<td>').append($('<a>').text('install').attr('href', '/dmx/repository/' + name + '/clone')));
+        }
+        $table.append($row);
+      };
+  $.each(repositories.items, function (i, r) { renderRepository(r) });
+});
